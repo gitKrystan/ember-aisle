0bcd9ff677a6d400c817536f48ff52114aae3011
Gregory Kanteres and Patrick Sullivan <gregkanteres@gmail.com>
2016-03-07 10:49:48 -0800

    add models for store, aisle, category, product, list

--- app/store/model.js
+++ app/store/model.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  brand: DS.belongsTo('brand', {async: true}),
+  lists: DS.hasMany('list', {async: true}),
+  aisles: DS.hasMany('aisle', {async: true}),
+  name: DS.attr('string'),
+  address: DS.attr('string')
 });
