19be089cbe08791d39cf3e8f9ead243d69a62810
Christopher Siems and Krystan Menne <csiems@gmail.com>
2016-03-07 15:13:56 -0800

    begin new-aisle form

---
+++ app/helpers/name-list.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export function nameList(params/*, hash*/) {
+  var nameArray = params[0].mapBy('name');
+  var nameList = nameArray.join(', ');
+
+  return nameList;
+}
+
+export default Ember.Helper.helper(categoryList);
---
+++ tests/unit/helpers/category-list-test.js
@@ -0,0 +1,10 @@
+import { categoryList } from 'aisle/helpers/category-list';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | category list');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = categoryList([42]);
+  assert.ok(result);
+});
