447a72994f2ccb0b7ec92fc85ee0b2dd36b7ba12
Gregory Kanteres and Patrick Sullivan <gregkanteres@gmail.com>
2016-03-07 11:24:42 -0800

    add new brand form

--- app/components/admin-forms/component.js
+++ app/components/admin-forms/component.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showBrandForm: false,
+  showStoreForm: false,
+
+  actions: {
+    showBrandForm() {
+      if (this.get('showBrandForm') === false) {
+        this.set('showBrandForm', true);
+      } else {
+        this.set('showBrandForm', false);
+      }
+    },
+
+    showStoreForm() {
+      if (this.get('showStoreForm') === false) {
+        this.set('showStoreForm', true);
+      } else {
+        this.set('showStoreForm', false);
+      }
+    },
+  }
 });
